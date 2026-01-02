138ef5a64082d1e34098dae39e6bf76905a267fc
Nikita <marknik139@gmail.com>
2026-01-02 22:09:14 +0300

    init

---
+++ build/src_pages_MainPage_MainPage_tsx.bd7f142a2176886f7893.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunkreact_course_ulbi"] = self["webpackChunkreact_course_ulbi"] || []).push([["src_pages_MainPage_MainPage_tsx"],{
+
+/***/ "./src/pages/MainPage/MainPage.tsx"
+/*!*****************************************!*\
+  !*** ./src/pages/MainPage/MainPage.tsx ***!
+  \*****************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+var MainPage = function () {
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: "main" }));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MainPage);
+
+
+/***/ }
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX01haW5QYWdlX01haW5QYWdlX3RzeC5iZDdmMTQyYTIxNzY4ODZmNzg5My5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztBQUFnRDtBQUNoRDtBQUNBLFlBQVksc0RBQUksVUFBVSxrQkFBa0I7QUFDNUM7QUFDQSxpRUFBZSxRQUFRLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZWFjdC1jb3Vyc2UtdWxiaS8uL3NyYy9wYWdlcy9NYWluUGFnZS9NYWluUGFnZS50c3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsganN4IGFzIF9qc3ggfSBmcm9tIFwicmVhY3QvanN4LXJ1bnRpbWVcIjtcbnZhciBNYWluUGFnZSA9IGZ1bmN0aW9uICgpIHtcbiAgICByZXR1cm4gKF9qc3goXCJkaXZcIiwgeyBjaGlsZHJlbjogXCJtYWluXCIgfSkpO1xufTtcbmV4cG9ydCBkZWZhdWx0IE1haW5QYWdlO1xuIl0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZVJvb3QiOiIifQ==
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './index.scss';
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {Suspense} from "react";
+
+export const App = () => {
+    return(
+        <div className='app'>
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+
+            <Suspense fallback={<div>Loading</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                </Routes>
+            </Suspense>
+        </div>
+    )
+}
