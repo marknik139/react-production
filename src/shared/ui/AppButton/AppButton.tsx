9e5701dc0d87c9449d0674b98daeae515680be0a
Nikita <marknik139@gmail.com>
2026-01-04 20:11:32 +0300

    add WP svg loader

---
+++ src/shared/ui/AppButton/AppButton.tsx
@@ -0,0 +1,30 @@
+import {classNames} from "shared/lib/classNames"
+import cls from './AppButton.module.scss'
+import {ButtonHTMLAttributes, FC, PropsWithChildren} from "react";
+
+export enum ThemeButton {
+    CLEAR = 'clear'
+}
+
+interface AppButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+    className?: string;
+    theme?: ThemeButton;
+}
+
+export const AppButton: FC<PropsWithChildren<AppButtonProps>> = (props: AppButtonProps) => {
+    const {
+        className,
+        children,
+        theme,
+        ...otherProps
+    } = props;
+
+    return (
+        <button
+            className={classNames(cls.AppButton, {}, [className, cls[theme]])}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+};``
---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,25 @@
+import {classNames} from "shared/lib/classNames"
+import {Theme, useTheme} from "app/providers/ThemeProvider";
+import cls from './ThemeSwitcher.module.scss'
+
+import {AppButton, ThemeButton} from "shared/ui/AppButton/AppButton";
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+
+interface ThemeSwitcherProps {
+    className?: string;
+}
+
+export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <AppButton
+            className={classNames(cls.ThemeSwitcher, {}, [className])}
+            theme={ThemeButton.CLEAR}
+            onClick={toggleTheme}
+        >
+            {theme === Theme.LIGHT ? <LightIcon/> : <DarkIcon/>}
+        </AppButton>
+    );
+};
